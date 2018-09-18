699c864b6d4eec0e17e8c1adec88c6d82ff5e9d1
jchmb <barelds.jochem@gmail.com>
2018-09-18 13:34:49 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 install_requires = ['aioredis']
 
 setup(
     name='redisobjects',
     version='0.1.0',
-    description='Object-oriented wrapper for aioredis. Meant for easy usage.',
+    description='Object-oriented wrapper for aioredis.',
+    long_description=long_description,
     author='Jochem Barelds',
     author_email='barelds.jochem@gmail.com',
     url='https://gitlab.com/jchmb/redisobjects',
