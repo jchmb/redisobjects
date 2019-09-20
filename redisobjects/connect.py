50c160902d8397fff04db1a2bc20fab519c807cb
jchmb <barelds.jochem@gmail.com>
2019-09-20 20:25:20 +0200

    Adde comments

--- redisobjects/connect.py
+++ redisobjects/connect.py
@@ -2,6 +2,12 @@ from .redis_manager import RedisManager
 
 from aioredis import create_connection
 
+'''
+Create a new Redis connection.
+:param string address
+:param dict **kwargs
+:returns RedisManager
+'''
 async def connect(address, **kwargs):
     connection = await create_connection(address, **kwargs)
     redis_manager = RedisManager(connection)
